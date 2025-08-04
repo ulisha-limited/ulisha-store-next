b9435156fa2d988fcf17356fb6e0d374dc14086e
Melvin Jones Repol <mrepol742@gmail.com>
2025-08-04 14:20:35 +0800

    fix: disable widenClientFileUpload and automaticVercelMonitors in Sentry configuration

--- next.config.ts
+++ next.config.ts
@@ -44,7 +44,7 @@ export default withSentryConfig(nextConfig, {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+  widenClientFileUpload: false,
 
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
@@ -59,5 +59,5 @@ export default withSentryConfig(nextConfig, {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  automaticVercelMonitors: false,
 });
