29d1cae47d6d6d91fb0c8df13b178da5abd1ba41
Melvin Jones Repol <mrepol742@gmail.com>
2025-08-04 14:00:27 +0800

    feat: update image remotePatterns configuration for better flexibility

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://xmrnhaktqjmnihzexpsd.supabase.co/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    unoptimized: true,
   },
-
-  productionBrowserSourceMaps: false,
+  
   reactStrictMode: true,
 
   eslint: {
