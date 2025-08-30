9f98c80a2ca6a79916f77962bee057dcb8fd6b0c
Melvin Jones Repol <mrepol742@gmail.com>
2025-08-30 21:34:32 +0800

    feat: added database typesafe

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,11 +2,12 @@
  * Copyright 2025 Ulisha Limited
  * Licensed under the Apache License, Version 2.0
  * See LICENSE file in the project root for full license information.
- */ 
+ */
 
-import { createBrowserClient } from "@supabase/ssr";
+ import { createBrowserClient } from "@supabase/ssr";
+ import { Database } from "../../supabase-types";
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+ export const supabase = createBrowserClient<Database>(
+   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+ );
--- src/lib/supabasebaseServer.ts
+++ src/lib/supabasebaseServer.ts
@@ -2,15 +2,16 @@
  * Copyright 2025 Ulisha Limited
  * Licensed under the Apache License, Version 2.0
  * See LICENSE file in the project root for full license information.
- */ 
+ */
 
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { Database } from "../../supabase-types";
 
 export function createSupabaseServerClient() {
   const cookieStorePromise = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
