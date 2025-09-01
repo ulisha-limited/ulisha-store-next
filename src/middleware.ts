af880faa8298797cf5792078e541f8ed85a9c71e
Melvin Jones Repol <mrepol742@gmail.com>
2025-09-01 22:10:55 +0800

    chore: temporarily disable prerender

--- src/middleware.ts
+++ src/middleware.ts
@@ -77,148 +77,148 @@ export async function middleware(request: NextRequest) {
    * Check if the request is from a bot and handle prerendering
    * This is useful for SEO and performance optimization.
    */
-  const userAgent = request.headers.get("user-agent");
+  // const userAgent = request.headers.get("user-agent");
 
-  const bots = [
-    "googlebot",
-    "yahoo! slurp",
-    "bingbot",
-    "yandex",
-    "baiduspider",
-    "facebookexternalhit",
-    "twitterbot",
-    "rogerbot",
-    "linkedinbot",
-    "embedly",
-    "quora link preview",
-    "showyoubot",
-    "outbrain",
-    "pinterest/0.",
-    "developers.google.com/+/web/snippet",
-    "slackbot",
-    "vkshare",
-    "w3c_validator",
-    "redditbot",
-    "applebot",
-    "whatsapp",
-    "flipboard",
-    "tumblr",
-    "bitlybot",
-    "skypeuripreview",
-    "nuzzel",
-    "discordbot",
-    "google page speed",
-    "qwantify",
-    "pinterestbot",
-    "bitrix link preview",
-    "xing-contenttabreceiver",
-    "chrome-lighthouse",
-    "telegrambot",
-    "oai-searchbot",
-    "chatgpt",
-    "gptbot",
-    "perplexity",
-    "claudeBot",
-    "amazonbot",
-    "integration-test", // Integration testing
-  ];
+  // const bots = [
+  //   "googlebot",
+  //   "yahoo! slurp",
+  //   "bingbot",
+  //   "yandex",
+  //   "baiduspider",
+  //   "facebookexternalhit",
+  //   "twitterbot",
+  //   "rogerbot",
+  //   "linkedinbot",
+  //   "embedly",
+  //   "quora link preview",
+  //   "showyoubot",
+  //   "outbrain",
+  //   "pinterest/0.",
+  //   "developers.google.com/+/web/snippet",
+  //   "slackbot",
+  //   "vkshare",
+  //   "w3c_validator",
+  //   "redditbot",
+  //   "applebot",
+  //   "whatsapp",
+  //   "flipboard",
+  //   "tumblr",
+  //   "bitlybot",
+  //   "skypeuripreview",
+  //   "nuzzel",
+  //   "discordbot",
+  //   "google page speed",
+  //   "qwantify",
+  //   "pinterestbot",
+  //   "bitrix link preview",
+  //   "xing-contenttabreceiver",
+  //   "chrome-lighthouse",
+  //   "telegrambot",
+  //   "oai-searchbot",
+  //   "chatgpt",
+  //   "gptbot",
+  //   "perplexity",
+  //   "claudeBot",
+  //   "amazonbot",
+  //   "integration-test", // Integration testing
+  // ];
 
-  const IGNORE_EXTENSIONS = [
-    ".js",
-    ".css",
-    ".xml",
-    ".less",
-    ".png",
-    ".jpg",
-    ".jpeg",
-    ".gif",
-    ".pdf",
-    ".doc",
-    ".txt",
-    ".ico",
-    ".rss",
-    ".zip",
-    ".mp3",
-    ".rar",
-    ".exe",
-    ".wmv",
-    ".doc",
-    ".avi",
-    ".ppt",
-    ".mpg",
-    ".mpeg",
-    ".tif",
-    ".wav",
-    ".mov",
-    ".psd",
-    ".ai",
-    ".xls",
-    ".mp4",
-    ".m4a",
-    ".swf",
-    ".dat",
-    ".dmg",
-    ".iso",
-    ".flv",
-    ".m4v",
-    ".torrent",
-    ".woff",
-    ".ttf",
-    ".svg",
-    ".webmanifest",
-  ];
-  const isBot =
-    userAgent && bots.some((bot) => userAgent.toLowerCase().includes(bot));
-  const isPrerender = request.headers.get("X-Prerender");
-  const pathname = new URL(request.url).pathname;
-  const extension = pathname.slice(((pathname.lastIndexOf(".") - 1) >>> 0) + 1);
+  // const IGNORE_EXTENSIONS = [
+  //   ".js",
+  //   ".css",
+  //   ".xml",
+  //   ".less",
+  //   ".png",
+  //   ".jpg",
+  //   ".jpeg",
+  //   ".gif",
+  //   ".pdf",
+  //   ".doc",
+  //   ".txt",
+  //   ".ico",
+  //   ".rss",
+  //   ".zip",
+  //   ".mp3",
+  //   ".rar",
+  //   ".exe",
+  //   ".wmv",
+  //   ".doc",
+  //   ".avi",
+  //   ".ppt",
+  //   ".mpg",
+  //   ".mpeg",
+  //   ".tif",
+  //   ".wav",
+  //   ".mov",
+  //   ".psd",
+  //   ".ai",
+  //   ".xls",
+  //   ".mp4",
+  //   ".m4a",
+  //   ".swf",
+  //   ".dat",
+  //   ".dmg",
+  //   ".iso",
+  //   ".flv",
+  //   ".m4v",
+  //   ".torrent",
+  //   ".woff",
+  //   ".ttf",
+  //   ".svg",
+  //   ".webmanifest",
+  // ];
+  // const isBot =
+  //   userAgent && bots.some((bot) => userAgent.toLowerCase().includes(bot));
+  // const isPrerender = request.headers.get("X-Prerender");
+  // const pathname = new URL(request.url).pathname;
+  // const extension = pathname.slice(((pathname.lastIndexOf(".") - 1) >>> 0) + 1);
 
-  if (
-    isPrerender ||
-    !isBot ||
-    (extension.length && IGNORE_EXTENSIONS.includes(extension))
-  ) {
-    return NextResponse.next();
-  } else {
-    // Check if request is coming from a bot
-    if (isBot) {
-      const newURL = `http://service.prerender.io/${request.url}`;
-      const newHeaders = new Headers(request.headers);
-      newHeaders.set("X-Prerender-Token", process.env.PRERENDER_TOKEN || "");
-      newHeaders.set("X-Prerender-Int-Type", "NextJS");
+  // if (
+  //   isPrerender ||
+  //   !isBot ||
+  //   (extension.length && IGNORE_EXTENSIONS.includes(extension))
+  // ) {
+  //   return NextResponse.next();
+  // } else {
+  //   // Check if request is coming from a bot
+  //   if (isBot) {
+  //     const newURL = `http://service.prerender.io/${request.url}`;
+  //     const newHeaders = new Headers(request.headers);
+  //     newHeaders.set("X-Prerender-Token", process.env.PRERENDER_TOKEN || "");
+  //     newHeaders.set("X-Prerender-Int-Type", "NextJS");
 
-      try {
-        const res = await fetch(
-          new Request(newURL, {
-            headers: newHeaders,
-            redirect: "manual",
-          }),
-        );
+  //     try {
+  //       const res = await fetch(
+  //         new Request(newURL, {
+  //           headers: newHeaders,
+  //           redirect: "manual",
+  //         }),
+  //       );
 
-        const responseHeaders = new Headers(res.headers);
-        responseHeaders.set("X-Redirected-From", request.url);
+  //       const responseHeaders = new Headers(res.headers);
+  //       responseHeaders.set("X-Redirected-From", request.url);
 
-        // Create a ReadableStream from the response body
-        const { readable, writable } = new TransformStream();
-        if (res.body) {
-          res.body.pipeTo(writable);
-        } else {
-          writable.close();
-        }
+  //       // Create a ReadableStream from the response body
+  //       const { readable, writable } = new TransformStream();
+  //       if (res.body) {
+  //         res.body.pipeTo(writable);
+  //       } else {
+  //         writable.close();
+  //       }
 
-        const response = new NextResponse(readable, {
-          status: res.status,
-          statusText: res.statusText,
-          headers: responseHeaders,
-        });
+  //       const response = new NextResponse(readable, {
+  //         status: res.status,
+  //         statusText: res.statusText,
+  //         headers: responseHeaders,
+  //       });
 
-        return response;
-      } catch (error) {
-        return NextResponse.next();
-      }
-    } else {
-      console.log("Not a bot, proceeding normally");
-    }
+  //       return response;
+  //     } catch (error) {
+  //       return NextResponse.next();
+  //     }
+  //   } else {
+  //     console.log("Not a bot, proceeding normally");
+  //   }
 
     return NextResponse.next();
   }
