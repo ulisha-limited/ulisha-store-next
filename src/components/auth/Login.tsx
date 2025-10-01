641dd78dbcacbf9952ee03a283194f05c9d75762
Melvin Jones Repol <mrepol742@gmail.com>
2025-10-02 07:25:35 +0800

    feat: remove recaptcha floating badge

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -299,6 +299,26 @@ export default function Login() {
               </div>
             </div>
           </div>
+
+          <p className="text-xs text-gray-500 mt-5">
+            This site is protected by reCAPTCHA and the Google{" "}
+            <a
+              href="https://policies.google.com/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://policies.google.com/terms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms of Service
+            </a>{" "}
+            apply.
+          </p>
         </div>
       </div>
     </div>
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -350,6 +350,26 @@ export default function Register() {
               </div>
             </div>
           </div>
+
+          <p className="text-xs text-gray-500 mt-5">
+            This site is protected by reCAPTCHA and the Google{" "}
+            <a
+              href="https://policies.google.com/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://policies.google.com/terms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms of Service
+            </a>{" "}
+            apply.
+          </p>
         </div>
       </div>
     </div>
