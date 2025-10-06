7a43e166e4db7c1ada8cd3adea38a3ea4023fc37
Elitexv <paulelite606@gmail.com>
2025-10-06 15:03:21 +0100

    /

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2025 Ulisha Limited
+
+ /* Copyright (c) 2025 Ulisha Limited
  *
  * This file is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
  * You may obtain a copy of the License at:
